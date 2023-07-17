84d0a7db9bceb3204669cc8aa411366b6d9e75a5
fkrydev <fikri.afa289@gmail.com>
2023-07-17 19:34:29 +0800

    switch to pages router

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Gebyar Matematika 2023</title>
+      </Head>
+      <div className='flex items-center justify-center h-screen'>
+        <div className='text-center'>
+          <h1 className='text-3xl font-bold'>Coming Soon</h1>
+          <h1 className='text-base'>Gebyar Matematika 2023</h1>
+        </div>
+      </div>
+    </>
+  )
+}
