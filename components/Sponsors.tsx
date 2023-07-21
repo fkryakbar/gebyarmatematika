6005139deb92c5d7934631ad41b520a26bcbd742
fkrydev <fikri.afa289@gmail.com>
2023-07-21 10:03:36 +0800

    add sponsors and media partner api

--- components/Sponsors.tsx
+++ components/Sponsors.tsx
@@ -1,4 +1,31 @@
+import Image from 'next/image';
+import useSWRImmutable from 'swr/immutable'
+
+const fetcher = (url: string) => fetch(url, {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+        'public-key': `${process.env.NEXT_PUBLIC_ETHINGS_TOKEN}`
+    }
+}).then(r => r.json())
+
+const Loading = () => {
+    return (
+        <>
+            <div className="w-20 h-20 bg-slate-200 rounded-lg  animate-pulse"></div>
+            <div className="w-20 h-20 bg-slate-200 rounded-lg  animate-pulse"></div>
+            <div className="w-20 h-20 bg-slate-200 rounded-lg  animate-pulse"></div>
+            <div className="w-20 h-20 bg-slate-200 rounded-lg  animate-pulse"></div>
+            <div className="w-20 h-20 bg-slate-200 rounded-lg  animate-pulse"></div>
+            <div className="w-20 h-20 bg-slate-200 rounded-lg  animate-pulse"></div>
+        </>
+    )
+}
+
 export default function Sponsors() {
+    const sponsors = useSWRImmutable(`${process.env.NEXT_PUBLIC_ETHINGS_API}/api/public/view/TiFe-1gE-SVa`, fetcher);
+    const mediaPartners = useSWRImmutable(`${process.env.NEXT_PUBLIC_ETHINGS_API}/api/public/view/QzTr-eOb-jCl`, fetcher);
+
     return (
         <>
             <section className="lg:w-[80%] w-full p-2 mx-auto my-10">
@@ -17,13 +44,17 @@ export default function Sponsors() {
                                 <div className="w-5 h-5 rounded-full bg-green-400"></div>
                             </div>
                         </div>
-                        <div className="mt-5 flex gap-3 flex-wrap animate-pulse">
-                            <div className="w-20 h-20 bg-slate-200 rounded-lg"></div>
-                            <div className="w-20 h-20 bg-slate-200 rounded-lg"></div>
-                            <div className="w-20 h-20 bg-slate-200 rounded-lg"></div>
-                            <div className="w-20 h-20 bg-slate-200 rounded-lg"></div>
-                            <div className="w-20 h-20 bg-slate-200 rounded-lg"></div>
-                            <div className="w-20 h-20 bg-slate-200 rounded-lg"></div>
+                        <div className="mt-5 flex gap-3 flex-wrap">
+                            {
+                                sponsors.isLoading ? (<Loading />) : sponsors.data.data.map((item: any, key: number) => {
+                                    return (
+                                        <div className='w-20' key={key}>
+                                            <Image width={100} height={100} src={`${process.env.NEXT_PUBLIC_ETHINGS_API}/api/public/download/${item.item_id}`} alt={`${item.name}`} />
+                                        </div>
+                                    )
+
+                                })
+                            }
                         </div>
                     </div>
                     <div className="basis-1/2 bg-white drop-shadow rounded-lg p-5 lg:mt-0 mt-5">
@@ -40,13 +71,17 @@ export default function Sponsors() {
                                 <div className="rounded-full float-right w-[25%] h-1 bg-[#005AAC] mt-2"> </div>
                             </div>
                         </div>
-                        <div className="mt-5 flex gap-3 flex-wrap animate-pulse">
-                            <div className="w-20 h-20 bg-slate-200 rounded-lg"></div>
-                            <div className="w-20 h-20 bg-slate-200 rounded-lg"></div>
-                            <div className="w-20 h-20 bg-slate-200 rounded-lg"></div>
-                            <div className="w-20 h-20 bg-slate-200 rounded-lg"></div>
-                            <div className="w-20 h-20 bg-slate-200 rounded-lg"></div>
-                            <div className="w-20 h-20 bg-slate-200 rounded-lg"></div>
+                        <div className="mt-5 flex gap-3 flex-wrap justify-center">
+                            {
+                                mediaPartners.isLoading ? (<Loading />) : mediaPartners.data.data.map((item: any, key: number) => {
+                                    return (
+                                        <div className='w-20' key={key}>
+                                            <Image width={100} height={100} src={`${process.env.NEXT_PUBLIC_ETHINGS_API}/api/public/download/${item.item_id}`} alt={`${item.name}`} />
+                                        </div>
+                                    )
+
+                                })
+                            }
                         </div>
                     </div>
                 </div>
@@ -54,3 +89,4 @@ export default function Sponsors() {
         </>
     )
 }
+
--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ethings-api.ninepmx.my.id",
+        port: "",
+        pathname: "/api/public/download/*",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
